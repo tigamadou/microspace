c3adb6b782bd487ff9781ed08f4f5117f694683f
genzaraky <amadou140589@gmail.com>
2021-03-25 11:54:19 +0100

    GameScene edited. Player can now move

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,18 +1,52 @@
 import 'phaser';
-import {Player} from '../Objects/Entities'
+import { Player } from '../Objects/Entities'
 export default class GameScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Game');
-    
+
   }
 
-  preload () {
+  preload() {
     // load images
     this.load.image('logo', 'assets/logo.png');
   }
 
-  create () {
-    
+  create() {
+    this.anims.create({
+      key: "sprEnemy0",
+      frames: this.anims.generateFrameNumbers("sprEnemy0"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprEnemy2",
+      frames: this.anims.generateFrameNumbers("sprEnemy2"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprExplosion",
+      frames: this.anims.generateFrameNumbers("sprExplosion"),
+      frameRate: 20,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: "sprPlayer",
+      frames: this.anims.generateFrameNumbers("sprPlayer"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.sfx = {
+      explosions: [
+        this.sound.add("sndExplode0"),
+        this.sound.add("sndExplode1")
+      ],
+      laser: this.sound.add("sndLaser")
+    };
 
     this.add.image(400, 300, 'bgImg');
     this.player = new Player(
@@ -20,6 +54,33 @@ export default class GameScene extends Phaser.Scene {
       this.game.config.width * 0.5,
       this.game.config.height * 0.5,
       "sprPlayer"
-    ); 
+    );
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  }
+
+  update() {
+    this.player.update();
+
+    if (this.keyW.isDown || this.keyUp.isDown) {
+      this.player.moveUp();
+    }
+    else if (this.keyS.isDown || this.keyDown.isDown) {
+      this.player.moveDown();
+    }
+
+    if (this.keyA.isDown || this.keyLeft.isDown) {
+      this.player.moveLeft();
+    }
+    else if (this.keyD.isDown || this.keyRight.isDown) {
+      this.player.moveRight();
+    }
   }
 };
