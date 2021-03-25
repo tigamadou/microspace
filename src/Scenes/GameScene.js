74c78ea972c4ca1ba31d8fb2dd168144d69f596a
genzaraky <amadou140589@gmail.com>
2021-03-25 14:09:45 +0100

    Player can now shoot

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -129,6 +129,20 @@ export default class GameScene extends Phaser.Scene {
     else if (this.keyD.isDown || this.keyRight.isDown) {
       this.player.moveRight();
     }
+
+    if (this.keySpace.isDown) {
+      this.player.setData("isShooting", true);
+    }
+    else {
+      this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+      this.player.setData("isShooting", false);
+    }
+
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+
+      enemy.update();
+    }
   }
 
   getEnemiesByType(type) {
