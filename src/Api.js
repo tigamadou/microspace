ee264301a8f53537d3365b9422d8afad82121d11
genzaraky <amadou140589@gmail.com>
2021-03-30 11:27:07 +0100

    eslint errors fixed

--- src/Api.js
+++ src/Api.js
@@ -4,7 +4,6 @@ require('regenerator-runtime/runtime');
 
 export default function Api() {
   const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
- 
 
   this.createGame = async (name) => {
     const response = await axios.post(baseUrl, { name })
@@ -21,12 +20,12 @@ export default function Api() {
       .then(response => response.data).catch(error => error);
     return data.resul;
   };
-  
+
   this.getScores = async (id) => {
     let arr = [];
     const url = `${baseUrl}/${id}/scores`;
     arr = await axios.get(url)
-    .then(response => response.data).catch(error => error);;
+      .then(response => response.data).catch(error => error);
     return arr;
   };
 }
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,36 +1,38 @@
-require('regenerator-runtime/runtime');
 import axios from 'axios';
-const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
-let id = 'Mvs2tq5oZt0NUQXM2uaF';
-const api = {
-    getScores: async() => {
-        let url = `${baseUrl}/${id}/scores`;
-        let arr = await axios.get(url)
-            .then(response => response.data).catch(error => error);;
-        return arr;
-    },
-    saveScrore: async(id, user, score)=>{
-        let url = `${baseUrl}/${id}/scores`;
 
-        let data = await axios.post(url, { user:user, score:score })
-          .then(response => response.data).catch(error => error);
-          
-        return data.result;
-    }
-}
+require('regenerator-runtime/runtime');
+
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const id = 'Mvs2tq5oZt0NUQXM2uaF';
+const api = {
+  getScores: async () => {
+    const url = `${baseUrl}/${id}/scores`;
+    const arr = await axios.get(url)
+      .then(response => response.data).catch(error => error);
+    return arr;
+  },
+  saveScrore: async (id, user, score) => {
+    const url = `${baseUrl}/${id}/scores`;
+
+    const data = await axios.post(url, { user, score })
+      .then(response => response.data).catch(error => error);
+
+    return data.result;
+  },
+};
 
 jest.mock('axios');
 test('should fetch result data with users and scores', async () => {
-    const dbData = { data: { result: [{ score: 1580, user: 'Amadou' }, { score: 260, user: 'Tigana' }] } };
-    axios.get.mockResolvedValue(dbData);    
-    const response = await api.getScores(id);
-    expect(response.result.length).toBe(2);
+  const dbData = { data: { result: [{ score: 1580, user: 'Amadou' }, { score: 260, user: 'Tigana' }] } };
+  axios.get.mockResolvedValue(dbData);
+  const response = await api.getScores(id);
+  expect(response.result.length).toBe(2);
 });
 
 test('should save the user score', async () => {
-    const returnData = { data: { result: 'You need to provide a valid score for the leaderboard' } };
-    axios.post.mockResolvedValue(returnData);    
-    const response = await api.saveScrore(id,'amadou',1500);    
-    
-    expect(response).toBe('You need to provide a valid score for the leaderboard');
+  const returnData = { data: { result: 'You need to provide a valid score for the leaderboard' } };
+  axios.post.mockResolvedValue(returnData);
+  const response = await api.saveScrore(id, 'amadou', 1500);
+
+  expect(response).toBe('You need to provide a valid score for the leaderboard');
 });
